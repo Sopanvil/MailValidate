15af21e7b010f17228c68412a10c1ccae3b3f00e
Sopanvil <lenivkov12@gmail.com>
2020-02-19 17:48:36 +0300

    threading

--- check_mail.py
+++ check_mail.py
@@ -1,39 +1,23 @@
-#=======================================
-
-# Логика -> 
-# Очень условное выполнение тестового задания
-# По апи тянем данные про пользователей какого-нибудь сервиса,
-# Передаем данные в mail_validate
-# Проходим по всем вытянутым данным
-# Выводим счетчик валидных адресов
-
-# Почему такое условие  mail_validate(r['some_email_id_in json']) == True
-# Нашел в библиотеках validate_mail - при вводе адреса возвращает нам True(валидный) и False(не валидный)
-
-#=======================================
-
-
+import threading
 import time
 import requests
+import multiprocessing
 
 
-# Получаем данные
-request = requests.get('some_adress')
-request = request.json()
-
-# Функция проверки
-def mail_validate(r):
-    #C параметром 'r' мы передавали бы параметры в функцию
+def mail_validate():
+    # Заглушка со слипом
     time.sleep(1)
 
-# Предположим, что мы тянем данные по API
-# Функция перебора, валидации и счетчика
-def validator():
-    counter_of_true = 0 # Счетчик
-    for r in request:
-        # Проверям, прошел ли адресс валидацию
-        if mail_validate(r['some_email_id_in json']) == True:
-            counter += 1
-    print(counter)
+#Начало выполнения
+start = time.time()
 
-validator()
+for i in range(100000):
+
+    t = threading.Thread(target=mail_validate,
+                         name='Thread{}'.format(i+1))
+    t.start()
+
+# Конец выполнения
+end = time.time()
+
+print('Время - ', end - start)
