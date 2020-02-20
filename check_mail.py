2b8088b112b2ce5e6b6058b881b5a55d27265488
Sopanvil <lenivkov12@gmail.com>
2020-02-20 23:48:52 +0300

    Maybe

--- check_mail.py
+++ check_mail.py
@@ -5,17 +5,25 @@ import multiprocessing
 
 
 def mail_validate():
-    # Заглушка со слипом
-    time.sleep(1)
+    # В пол часа максимум 1800
+    for i in range(1800):
+        time.sleep(1)
 
 #Начало выполнения
 start = time.time()
 
-for i in range(100000):
+thread_list = []
+
+#1кк / 1800 ~= 556
+for i in range(556):
 
     t = threading.Thread(target=mail_validate,
                          name='Thread{}'.format(i+1))
+    thread_list.append(t)
     t.start()
+
+for i in thread_list:
+    i.join()
 
 # Конец выполнения
 end = time.time()
