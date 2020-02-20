063698cf22e932a9f3090e98ebc73c850a615318
Sopanvil <lenivkov12@gmail.com>
2020-02-21 00:24:52 +0300

    idknow

--- check_mail.py
+++ check_mail.py
@@ -4,7 +4,7 @@ import time
 
 def mail_validate():
     # В пол часа максимум 1800
-    for i in range(1800):
+    for i in range(10):
         time.sleep(1)
 
 #Время начала выполнения
@@ -14,14 +14,15 @@ t_l = []
 
 #1кк / 1800 ~= 556
 for i in range(556):
-
     t = threading.Thread(target=mail_validate,
-                         name='Thread{}'.format(i+1))
+                            name='Thread-{}'.format(i+1))
     t_l.append(t)
     t.start()
 
 for i in t_l:
     i.join()
+    if i.is_alive() != False:
+        print('{} - Что-то пошло не так'.format(i.name))
 
 # Конец выполнения
 end = time.time()
