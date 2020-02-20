c1f6fb6bf376a1a237192530a05c6e00c3355e3a
Sopanvil <lenivkov12@gmail.com>
2020-02-21 00:25:59 +0300

    Messages

--- check_mail.py
+++ check_mail.py
@@ -7,15 +7,16 @@ def mail_validate():
     for i in range(10):
         time.sleep(1)
 
-#Время начала выполнения
+
+# Время начала выполнения
 start = time.time()
 
 t_l = []
 
-#1кк / 1800 ~= 556
+# 1кк / 1800 ~= 556
 for i in range(556):
     t = threading.Thread(target=mail_validate,
-                            name='Thread-{}'.format(i+1))
+                         name='Thread-{}'.format(i+1))
     t_l.append(t)
     t.start()
 
